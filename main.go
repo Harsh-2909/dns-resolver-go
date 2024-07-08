7ed69357245bd8927f70ae82d11833b357b51cd4
Harsh Agarwal <agarwal.harsh@senrysa.com>
2024-07-08 19:13:08 +0530

    feat: Creates a UDP Socket which listens and returns the data sent as output

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr := &net.UDPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 3000,
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	fmt.Printf("Listening on %s\n", addr)
+
+	for {
+		buffer := make([]byte, 1024)
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Received", string(buffer[:n]), "from", addr)
+		_, err = conn.WriteToUDP(buffer[1:n], addr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
